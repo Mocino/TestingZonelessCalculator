3a35f4e981c6fc773771396b97b04ddb445d432b
ederson <egarciaf2@gmail.com>
2025-03-24 08:37:42 -0600

    feat: pruebas unitarias de Services

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,29 +1,41 @@
-import { TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
+  let fixture: ComponentFixture<AppComponent>;
+  let compiled:  HTMLElement;
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [AppComponent],
     }).compileComponents();
+
+    fixture = TestBed.createComponent(AppComponent);
+    compiled = fixture.nativeElement as HTMLElement;
   });
 
   it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'ZonelessCalculator' title`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('ZonelessCalculator');
+  it('should render title', () => {
+    expect(compiled.querySelector('router-outlet')).not.toBeNull();
   });
 
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, ZonelessCalculator');
-  });
+
+  it('should render router.outlet wrapped with css clases', () => {
+
+    const divElement = compiled.querySelector('div');
+    const mustHaveClasses =
+    'min-w-screen min-h-screen bg-slate-700 flex items-center justify-center px-5 py-5'
+    .split('');
+    const divClasses = divElement?.classList.value.split('');
+
+    expect(divElement).not.toBeNull();
+
+    mustHaveClasses.forEach((className) => {
+      expect(divClasses).toContain(className);
+    })
+  })
 });
